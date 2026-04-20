a384c7b5bf77946343a8826552056bbe4929f57d
AsmitaMallick <asmitamallick2@gmail.com>
2026-04-20 10:36:21 +0530

    prduction changes

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -59,9 +59,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:5173',
-]
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'config.urls'
 
